5917766143b551c1ec67ffcc4758c572cefc039e
Nicholas Gonzalez <nicky93@gmail.com>
2014-12-23 14:30:26 -0500

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -114,22 +114,22 @@ class ApplicationController < ActionController::Base
     singleShareData[:earnedShares] = 0
 
     if share.end_date.to_date == due_date
-      singleShareData[:daysVested] = (due_date - Time.at(share.offer_date).to_date).to_i + 1
+      singleShareData[:daysVested] = (due_date - Time.at(share.date_signed).to_date).to_i
     elsif share.end_date.to_date < share.cliff_date.to_date
       return singleShareData
     else
-      singleShareData[:daysVested] = (Time.at(share.end_date).to_date - Time.at(share.offer_date).to_date).to_i
+      singleShareData[:daysVested] = (Time.at(share.end_date).to_date - Time.at(share.date_signed).to_date).to_i
     end
 
-    if Time.at(share.offer_date).to_date != due_date && Time.at(share.offer_date).to_date < due_date
-      singleShareData[:dailyIncrease] = dailyShareIncrease = share.shares / ((due_date - Time.at(share.offer_date).to_date).to_i)
+    if Time.at(share.offer_date).to_date < due_date && Time.at(share.date_signed).to_date < due_date
+      singleShareData[:dailyIncrease] = dailyShareIncrease = share.shares / ((due_date - Time.at(share.date_signed).to_date).to_i)
     else
       singleShareData[:dailyIncrease] = dailyShareIncrease = share.shares
     end
 
     singleShareData[:totalShares] = share.shares
 
-    if Time.at(share.offer_date).to_date < due_date
+    if Time.at(share.offer_date).to_date < due_date && Time.at(share.offer_date).to_date < due_date
       singleShareData[:earnedShares] = singleShareData[:daysVested] * dailyShareIncrease
     elsif Time.at(share.offer_date).to_date >= due_date
       singleShareData[:earnedShares] = share.shares
