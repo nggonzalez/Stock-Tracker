1bd7d5c0450c8b15fa599feaa0d42bfbbc7218c6
Nicholas Gonzalez <nicky93@gmail.com>
2015-01-31 16:51:10 -0500

    Removing print outs from report_controller.rb

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -1,8 +1,6 @@
 class API::ReportsController < ApplicationController
 
   def bug
-    puts request
-    puts request.POST
     Reports.bug_email(request.POST['error'], request.POST['feedback'], get_user).deliver
     head :no_content
   end
